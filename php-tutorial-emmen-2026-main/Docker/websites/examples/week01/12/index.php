fe47529aa8a227d0b40207d6838ae9594c08b4ba
hunain <hunain.mukhtar.mazoor@student.nhlstenden.com>
2026-09-15 10:51:25 +0200

    Assignment-1-php-3-parts

--- php-tutorial-emmen-2026-main/Docker/websites/examples/week01/12/index.php
+++ php-tutorial-emmen-2026-main/Docker/websites/examples/week01/12/index.php
@@ -2,8 +2,33 @@
 
 # let's setup some variables to display later (this is comment by the way :))
 
-$name = "Martin";
-$surname = "Molema";
+$name = "Marci";
+$surname = "David";
+$year = "2025";
+
+function calc(int $a, int $b) {
+    $x = $a * $b;
+    return $x;
+}
+
+function verification(int $a){
+
+    $result = "";
+
+    if ($a == 2026) {
+            $result = "Yesss";
+    } else if ($a == 2025) {
+            $result = "It is 2025";
+    } else {
+            $result = "No";
+    }
+
+    return $result;
+}
+
+$fecha = verification($year);
+$multi = calc(2,6);
+
 
 ?><!doctype html>
 <html lang="en">
@@ -11,13 +36,7 @@ $surname = "Molema";
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>PHP Example 2</title>
-    <style>body {
-            font-family: sans-serif
-        }
-        h1 {
-            font-size: 1.3rem;
-        }
-    </style>
+    <link rel="sheet" href="style.css">
 </head>
 <body>
 <h1>Welcome <?= $name ?> <?= $surname ?></h1>
@@ -26,14 +45,14 @@ $surname = "Molema";
     5 and 6 to display your own name.
 </p>
 <p>
-    Today is <?= date("l, j M Y") ?>.
+    Today is <?= date("l jS \of F Y h:i:s A") ?>.
 </p>
 <p>
-    283 times 4937 = <?= 283 * 4937 ?>
+    283 times 4937 = <?= $multi ?>
 </p>
 
 <p>
-    Is it 2026? <?= date("Y") == "2026" ? "yes" : "no" ?>
+    Is it 2026? <?= $fecha ?>
 </p>
 </body>
 </html>
